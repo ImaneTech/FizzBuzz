f88aa5f72669109b3a09c426605f41536aaa09e9
ImaneTech <imane.zouine100@gmail.com>
2024-12-19 17:01:56 +0100

    Etat initial de project

---
+++ src/test/java/com/controle/tdd/FizzBuzzTest.java
@@ -0,0 +1,11 @@
+package com.controle.tdd;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTest {
+
+    @Test
+    void fizzBuzz_de_1_devrait_retourner_1(){
+        Assertions.fail("echec cycle 1");
+    }
+}
